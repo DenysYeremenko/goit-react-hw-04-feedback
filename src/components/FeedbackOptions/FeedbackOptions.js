a53eca4859a89b57749999f77266e0af6daaeadb
Denys Yeremenko <95313654+DenysYeremenko@users.noreply.github.com>
2022-08-28 08:56:57 +0200

    hw fixes

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,22 +1,15 @@
-import { Component } from "react"
 import { Box } from "components/Box";
 import { FeedbackButton } from "./FeedbackOptions.styled";
 import PropTypes from 'prop-types';
 
-export class FeedbackOptions extends Component {
-    static propTypes = {
-        options: PropTypes.arrayOf(PropTypes.string).isRequired,
-        onLeaveFeedback: PropTypes.func.isRequired
-    }
-
-    render() {
-        const {
-            options,
-            onLeaveFeedback
-        } = this.props
-        
+export const FeedbackOptions = ({options, onLeaveFeedback}) => {
     return (<Box display="flex">
         {options.map(option => <FeedbackButton key={option} onClick={onLeaveFeedback}>{option.toUpperCase()}</FeedbackButton>)}    
     </Box>)
-    }
+    
+}
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired
 }
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,18 +1,15 @@
-import { Component } from "react"
 import { SectionHeader } from "./Section.styled"
 import { Box } from "components/Box"
 import PropTypes from 'prop-types';
 
-export class Section extends Component {
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        children: PropTypes.object.isRequired
-    }
-    render() {
-        const {title, children} = this.props
+export const Section = ({title, children}) => { 
         return <Box as='section' display="flex" alignItems="center" flexDirection="column">
                     <SectionHeader>{title}</SectionHeader>
                     {children}
                 </Box>
-    }
+}
+
+Section.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.object.isRequired
 }
